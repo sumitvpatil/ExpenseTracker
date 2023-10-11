0496040442b0dd13ee7332b596a989be652fba1a
sumitpatil4 <sumit.patil@accolitedigital.com>
2023-10-11 14:31:31 +0530

    Changed to previous layout

--- src/App.js
+++ src/App.js
@@ -18,9 +18,9 @@ function App() {
         <Route path='/' element={<Homepage/>}></Route>
         <Route path='/login' element={<Login/>}></Route>
         <Route path='/register' element={<Register/>}></Route>
-        <Route path='/dashboard1' element={<Dashboard/>}></Route>
-        <Route path='/dashboard' element={<UserHome/>}></Route>
-        <Route path='/expense' element={<Expense/>}></Route>
+        <Route path='/dashboard' element={<Dashboard/>}></Route>
+        {/* <Route path='/dashboard' element={<UserHome/>}></Route>
+        <Route path='/expense' element={<Expense/>}></Route> */}
       </Routes>
     </GlobalProvider>
   );
